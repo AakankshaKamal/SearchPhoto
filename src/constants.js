01ee94036c709efdf7ea7ab4e7341de533a80b89
AakankshaKamal <aakankshakamal.post@gmail.com>
2021-05-02 14:06:55 +0530

    Final issue resolved

--- src/constants.js
+++ src/constants.js
@@ -2,9 +2,9 @@ const constants = {
 
 	
 	BASE_URL:
-		"https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=add your api key &format=json&nojsoncallback=1",
+		"https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=49962b6e28eb9e701524a771bf1c9fd5&format=json&nojsoncallback=1",
 	STORAGE_KEY: "flicker_search_queries",
-	DEFAULT_URL:"https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=add your api key &format=json&nojsoncallback=1"
+	DEFAULT_URL:"https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=49962b6e28eb9e701524a771bf1c9fd5&format=json&nojsoncallback=1"
 };
 
 export default constants;
